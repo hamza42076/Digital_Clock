8a0a83d2bbfb39c754692b7b9e08035cdbbef482
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2025-04-12 12:47:36 +0500

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+let hr = document.querySelector(".hr")
+let min = document.querySelector(".min")
+let sec = document.querySelector(".sec")
+let day = document.querySelector(".day")
+let am_pm = document.querySelector(".am-pm")
+
+let days_week= new Date()
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wedenesday", "Thursday", "Friday", "Saturday"];
+day.innerText =dayNames[days_week.getDay()]
+
+setInterval(function(){
+    let newDate = new Date()
+hr.innerText =newDate.getHours() +":" 
+min.innerText = newDate.getMinutes() +":" 
+sec.innerText = newDate.getSeconds() +":" 
+if(newDate.getHours() > 12){
+    am_pm.innerText = "PM"
+
+}
+
+},1000)
