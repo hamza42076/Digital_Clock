bffdda4790f620e938403a8a21508013671ec6e5
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2025-04-15 19:31:09 +0500

    first commit

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ let min = document.querySelector(".min")
 let sec = document.querySelector(".sec")
 let day = document.querySelector(".day")
 let am_pm = document.querySelector(".am-pm")
+let month = document.querySelector(".month")
+let date = document.querySelector(".date")
+let year = document.querySelector(".year")
+
 
 let days_week= new Date()
 var dayNames = ["Sunday", "Monday", "Tuesday", "Wedenesday", "Thursday", "Friday", "Saturday"];
 day.innerText =dayNames[days_week.getDay()]
+let months =["jan","feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"]
 
 setInterval(function(){
     let newDate = new Date()
 hr.innerText =newDate.getHours() +":" 
 min.innerText = newDate.getMinutes() +":" 
 sec.innerText = newDate.getSeconds() +":" 
+month.innerText = months[newDate.getMonth()] +" : " 
+date.innerText = newDate.getDate() +" : " 
+year.innerText = newDate.getFullYear()
+
 if(newDate.getHours() > 12){
     hr.innerText = newDate.getHours() -12
     am_pm.innerText = "PM"
@@ -24,6 +33,18 @@ else if (newDate.getHours() === 0){
 
 }
 else{
-    hr.innerText =  newDate.getHours();
+    hr.innerText =  newDate.getHours() +":";
 }
+
 },1000)
+// function fac(n) {
+//     if (n === 1 || n === 0){
+//         return 1;
+//     }
+//     return n * fac(n-1)
+// }
+// let h = fac(6)
+// console.log(h);
+
+
+
